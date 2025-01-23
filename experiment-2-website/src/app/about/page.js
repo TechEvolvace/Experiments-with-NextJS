532ac87be744cfaba3ca64c0f9d30ece0bf74fbe
TechEvolvace <EvolvaceTechnical.PhucDinh@proton.me>
2025-01-22 23:45:35 -0600

    Successfully implemented the route to the About page, making this NextJS application a successful multi-page website that only just needs navigation links!

---
+++ experiment-2-website/src/app/about/page.js
@@ -0,0 +1,9 @@
+import styles from "../page.module.css";
+
+export default function About() {
+    return (
+      <div className={styles.page}>
+          <h1>About page for NextJS Experiment 2 Website!</h1>
+      </div>
+    );
+}
