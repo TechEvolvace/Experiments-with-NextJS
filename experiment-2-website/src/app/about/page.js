374d9ac82c48cad883ad3754cdc0c9b688aaa35b
TechEvolvace <EvolvaceTechnical.PhucDinh@proton.me>
2025-01-22 23:47:57 -0600

    Added new content about this experiment website with details about this experiment website!

--- experiment-2-website/src/app/about/page.js
+++ experiment-2-website/src/app/about/page.js
@@ -4,6 +4,14 @@ export default function About() {
     return (
       <div className={styles.page}>
           <h1>About page for NextJS Experiment 2 Website!</h1>
+
+          <br></br>
+          <p>This NextJS Experiment 2 will be a multi-page application written by NextJS!
+            To create this multi-page website with NextJS, you first create a new directory
+            with a specific name under the src/app directory and then create a page.js file 
+            under that new directory! Then you define a new component with content to display 
+            for the new page of the website and export it!
+          </p>
       </div>
     );
 }
