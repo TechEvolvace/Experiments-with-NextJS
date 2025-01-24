15b8c89eeee60601a1b8c6efbcb80aaf59820ce6
TechEvolvace <EvolvaceTechnical.PhucDinh@proton.me>
2025-01-23 23:45:07 -0600

    Added goals for this experiment in landing page!

--- experiment-2-website/src/app/page.js
+++ experiment-2-website/src/app/page.js
@@ -4,6 +4,13 @@ export default function Home() {
   return (
     <div className={styles.page}>
         <h1>NextJS application for Experiment 2!</h1>
+
+        <h2>Goals</h2>
+        <ul>
+          <li>Implement a Navbar with same design on landing page and the About page</li>
+          <li>Add navigation link to route to About page</li>
+          <li>Implement unique page styling for landing page and the About page</li>
+        </ul>
     </div>
   );
 }
